cb60a5b354712b08cb808b95683803f80a76fc0b
bridgetorr17 <bridgetorr1902@gmail.com>
2025-05-17 19:42:33 -0400

    redoing 7kyu

--- searchSolution.js
+++ searchSolution.js
@@ -1,19 +1,5 @@
 function solution(fullText, search) {
     // Do your thing.
-      let count = 0; //number of instances 
-      let i = 0; //iterator
-  
-      //look for instances of the search
-      //exit the while look when the chars left in the string are less than the search string
-      while (i <= fullText.length - search.length) {
-          if (fullText.substring(i, i + search.length) === search) {
-              count++;
-              i += search.length; // avoid overlap
-          } else {
-              i++;
-          }
-      }
-      
-      return count;
+    let count = fullText.split(search);
+    return count.length - 1;
   }
-  
