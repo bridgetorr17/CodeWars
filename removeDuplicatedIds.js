c09b89526ba511c3b14cd7d11af2c7babc5011a2
bridgetorr17 <bridgetorr1902@gmail.com>
2025-07-07 16:48:41 -0400

    6kyu

--- removeDuplicatedIds.js
+++ removeDuplicatedIds.js
@@ -1,17 +1,27 @@
 const removeDuplicateIds = (obj) => {
+  //sort the keys in order first
   const sortedKeys = Object.keys(obj)
-                         .sort((a, b) => parseInt(a) - parseInt(b));
+                           .sort((a, b) => parseInt(a) - parseInt(b));
   
+  //make a sorted object from the sorted keys
   const sortedObj = sortedKeys.reduce((acc, key) => {
     acc[key] = obj[key];
     return acc
   }, {})
   
-  let keys = Object.keys(sortedObj);
-
+  let ids = []; //tracking all values in obj
+  
   //iterate backwards through object
-  for (let i=keys.length; i>0; i--){
-    console.log(sortedObj[i])
+  for (let i=sortedKeys.length; i>0; i--){
+    sortedObj[sortedKeys[i-1]] = sortedObj[sortedKeys[i-1]].filter((letter) => {
+      if (!ids.includes(letter)){
+        ids.push(letter);
+        return true;
+      } else{
+        return false;
+      }
+    });
   }
-  console.log('done')
+
+  return sortedObj;
 };
