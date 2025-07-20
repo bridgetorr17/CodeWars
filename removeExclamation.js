0a1052950a2a27efa9452e961369011300f16146
bridgetorr17 <bridgetorr1902@gmail.com>
2025-07-20 09:08:07 -0400

    7kyu

---
+++ removeExclamation.js
@@ -0,0 +1,24 @@
+function removeExclamation(str) {
+    //separate string into array by spaces
+    let arr = str.split(' ');
+
+    let arrRet = arr.map((word) => {
+        let wordArr = word.split('');
+        for(let i=wordArr.length-1; i>=0; i--){
+            if(wordArr[i] === '!'){
+                wordArr.pop();
+            } else break;
+        }
+
+        return wordArr.join('');
+    })
+
+    return arrRet.join(' ');
+
+    
+}
+
+console.log(removeExclamation('Exclamat!on!! !!E!'), 'should return: Exclamat!on !!E')
+console.log(removeExclamation('!Exclamat!on!'), 'should return: !Exclamat!on')
+console.log(removeExclamation('!Exclamat!on'), 'should return: !Exclamat!on')
+console.log(removeExclamation('HI!!!!!'), 'should return: HI')
